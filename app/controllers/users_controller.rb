d59eba43e57e9f9a7e711eae29df135f4649fed9
KerronKing <king_of_palmiste@hotmail.com>
2019-11-13 16:18:23 -0400

    Edit create and show actions in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to "show"
+    else
+      render "new"
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  get '/signup', to: 'users/new'
   get 'users/create'
   get 'users/show'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
