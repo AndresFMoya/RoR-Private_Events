3477b95d371fb3a2176f4f44b997828968045c60
AndresFMoya <andresfmoya@gmail.com>
2019-11-13 16:59:34 -0500

    chore: add the association between creator and events

--- db/migrate/20191113214835_create_events.rb
+++ db/migrate/20191113214835_create_events.rb
@@ -3,8 +3,10 @@ class CreateEvents < ActiveRecord::Migration[6.0]
     create_table :events do |t|
       t.datetime :date
       t.text :description
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
+    add_index :events, [:user_id, :created_at]
   end
 end
