1283f4f486d90e285cea1360bc808b6567425c62
AndresFMoya <andresfmoya@gmail.com>
2019-11-13 17:35:02 -0500

    chore: generate controller Events

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,16 @@
+class EventsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'events/new'
+  get 'events/create'
+  get 'events/show'
+  get 'events/destroy'
+  get 'events/index'
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
