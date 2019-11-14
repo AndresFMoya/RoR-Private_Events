3144bcb3fd233542387eb72e1466e8f3c5aed038
KerronKing <king_of_palmiste@hotmail.com>
2019-11-14 12:50:34 -0400

    Chore: update event and user model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  has_many :users, through: :attended_events, foreign_key: "event_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: "creator_id"
+  has_many :attended_events, through: :attended_events,
+                             foreign_key: "attendee_id"
 end
