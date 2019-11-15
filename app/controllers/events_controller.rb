98a6092f5193696d123d79d3fbcf9d7ceb66beb7
KerronKing <king_of_palmiste@hotmail.com>
2019-11-15 15:48:10 -0400

    Update: edit event_params method to save date information to db

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -40,6 +40,6 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:description)
+    params.require(:event).permit(:description, :date)
   end
 end
