f2c7588acfd34e199adaf8160c1d29af5afdf115
KerronKing <king_of_palmiste@hotmail.com>
2019-11-15 16:51:55 -0400

    Feat: add presence and length validity rspec tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   include EventsHelper
   has_many :events, foreign_key: "creator_id", dependent: :destroy
   has_many :attendedevents, class_name: "AttendedEvent", foreign_key: "attendee_id", dependent: :destroy
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 40 }
 end
