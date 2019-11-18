efdebcd890b4563206164a578a62def0990fe770
KerronKing <king_of_palmiste@hotmail.com>
2019-11-18 18:54:29 -0400

    Update: edit current_user method in the sessions helper file

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,7 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
+      @current_user = User.find_by(id: user_id)
     end
   end
 
