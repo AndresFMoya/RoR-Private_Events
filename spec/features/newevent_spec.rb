09df19beee7df5c6f5de2bfa56627d276012be14
AndresFMoya <andresfmoya@gmail.com>
2019-11-18 17:15:56 -0500

    test: add integration test to event creation

--- spec/features/newevent_spec.rb
+++ spec/features/newevent_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.feature "Newevents", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+  before :each do
+    @user=User.create(name: 'User', email: 'User@example.com')
+    @event=@user.events.build(description: 'Test Event')   
+  end 
+
+  scenario 'User Create an Event' do
+    visit '/login'
+    fill_in "Name", with: "User"
+    click_button "Sign in"
+    visit '/events/new'
+    fill_in "Description", with: "This is my event"
+    expect(page).to have_text(@user.name)
+  end
 end
