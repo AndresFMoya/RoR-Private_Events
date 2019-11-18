a4a075bfea4c939bc2792ba0781f81455b0bddf9
AndresFMoya <andresfmoya@gmail.com>
2019-11-18 17:09:41 -0500

    test: add integration test to authentication

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "Logins", type: :feature do
+
+  before :each do
+    User.create(name: 'User', email: 'User@example.com')
+  end 
+
+  scenario 'User Log in' do
+    visit '/login'
+    fill_in "Name", with: "User"
+    click_button "Sign in"
+    expect(page).to have_text("Events")
+  end
+
+  scenario 'Invalid User not Log in' do
+    visit '/login'
+    fill_in "Name", with: "InvalidUser"
+    click_button "Sign in"
+    expect(page).to have_text("Invalid Username")
+  end
+end
--- spec/requests/requests_spec.rb
+++ spec/requests/requests_spec.rb
@@ -1,21 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "UserLogin", type: :request do
-
-  let(:user) { users(:laura) }
+RSpec.describe "Requests", type: :request do
 
   context "Requests" do
     it "Get Login Page" do
       get login_path
-      post login_path, session: { name: user.name, email:user.email }
-
-      expect(response).to redirect_to(user_path)
+      expect(response).to have_http_status(200)
     end
 
     it "Get Sign up Page" do
       get signup_path
       expect(response).to have_http_status(200)
     end
-
   end
 end
