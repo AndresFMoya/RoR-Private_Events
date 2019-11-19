bb6e49cb805dff8871d4b35910cdf6faaef69865
AndresFMoya <andresfmoya@gmail.com>
2019-11-19 10:51:39 -0500

    fix: edit users controller to redirect to user after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render 'show'
+      log_in @user
+      redirect_to @user
     else
       render 'new'
     end
